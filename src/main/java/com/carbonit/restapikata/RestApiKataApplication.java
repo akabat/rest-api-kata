8c6f0dc9a9fbeec418bf83194623a4272142c673
Andrzej KABAT <andrzej.kabat@carbon-it.com>
2020-07-27 11:01:22 +0200

    Initial commit

---
+++ src/main/java/com/carbonit/restapikata/RestApiKataApplication.java
@@ -0,0 +1,13 @@
+package com.carbonit.restapikata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carbonit/restapikata/RestApiKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbonit.restapikata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
